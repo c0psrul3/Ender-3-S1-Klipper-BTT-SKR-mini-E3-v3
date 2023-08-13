c41f2f0245f8b1b77c29c9f817dc47e7e5a4569a
c0psrul3 <c0psrul3@pm.me>
2023-08-13 11:01:35 -0400

    updated config_sync script

--- Ender_3_S1/config_sync.sh
+++ Ender_3_S1/config_sync.sh
@@ -1,12 +1,44 @@
 #
-# backup "printer_data/config" from ${REMOTE_HOST}
+# Script to sync "printer_data/config" to/from a defined KLIPPER_HOST
 #
 
-typeset -g REMOTE_HOST LOCAL_BASEDIR LOCAL_PATH
-typeset DRYRUN=${DRYRUN:-1}
-typeset VERBOSE=${VERBOSE:-1}
+typeset -g KLIPPER_HOST LOCAL_BASEDIR LOCAL_PATH
+
+# determine VERBOSE selection
+typeset -g VERBOSE=${VERBOSE:-1}
+if [[ ${VERBOSE:=1} == 1 ]] ; then
+    printf "[INFO] Verbose Mode\n" >&2
+else
+    unset VERBOSE
+fi
+
+# determine DRYRUN selection
+typeset -g DRYRUN=${DRYRUN:-1}
+if [[ ${DRYRUN:=1} == 0 ]] ; then
+    unset DRYRUN
+    printf "[WARN] This is NOT a dryrun! will overwrite files!\n" >&2
+else
+    printf "[INFO] This IS a dryrun! will NOT overwrite files!\n" >&2
+fi ; sleep 1
+
+# set default option
+typeset _opt="backup"
+typeset _files=()
+# get option selection from input parameter
+if [[ -n $1 ]] ; then
+    ##
+    #TODO: allow for specific list of files as non-option parameter
+    ##
+    # check format of option
+    [[ "${1#--}" == "$1" ]] && printf "[ERROR] Invalid Parameter, Option must be specified as a flag\n" >&2 && return 1
+    _opt="${1#--}"
+fi
+
+# hold horses
+sleep 1
 
 
+# usage / help
 USAGE="
 USAGE:    ./$0 [--backup|--update|--help]
 
@@ -22,7 +54,7 @@ OPTIONS:
 
 ENVIRONMENT:
 
-    REMOTE_HOST
+    KLIPPER_HOST
 
     LOCAL_BASEDIR
 
@@ -35,62 +67,63 @@ ENVIRONMENT:
 "
 
 
-: ${REMOTE_HOST:=mainsail}
+: ${KLIPPER_HOST:=mainsail}
 : ${LOCAL_BASEDIR:=${PWD}/}
 
 : ${LOCAL_PATH:=${LOCAL_BASEDIR%/}/./printer_data}
 
 # check if LOCAL_BASEDIR/printer_data exists
-if [[ -e "${LOCAL_BASEDIR%/}/./printer_data/" ]] && [[ -d "${LOCAL_BASEDIR%/}/./printer_data/" ]] ; then
-        
-else
+if [[ ! -e "${LOCAL_BASEDIR%/}/./printer_data/" ]] || [[ ! -d "${LOCAL_BASEDIR%/}/./printer_data/" ]] ; then
     printf "[ERROR] Directory NOT FOUND: %s\n" "${LOCAL_PATH}" >&2
     return 1
 fi
+printf "[OK] Found Directory: %s\n" "${LOCAL_PATH}" >&2
 
 
+#
+# Function:  _backup_remote_config()
+#
 function _backup_remote_config() {
-    local DRYRUN=1
-    local VERBOSE=1
-    [[ ${1} == "-n" ]] || unset DRYRUN
-    [[ ${1} == "-n" ]] || unset DRYRUN
+    [[ -z $DRYRUN ]] && printf "${VERBOSE:+\n}[WARN] Performing BACKUP of remote config!\n${VERBOSE:+\n}"   >&2
+    [[ -n $DRYRUN ]] && printf "${VERBOSE:+\n}[DRYRUN] Performing BACKUP of remote config!\n${VERBOSE:+\n}" >&2
     rsync ${DRYRUN:+-n} ${VERBOSE:+-v} -R -acb --copy-links \
         --exclude="*.swp" \
         --exclude="*.bkp" \
         --exclude="*.BAK" \
+        --exclude="*~" \
         --exclude="printer-$(date +%Y)*.cfg" \
-        ${REMOTE_HOST}:printer_data/./config \
+        ${KLIPPER_HOST}:printer_data/./config \
         ${LOCAL_BASEDIR%/}/./printer_data/
 }
 
 
+#
+# Function:  _update_remote_config()
+#
 function _update_remote_config() {
-    [[ ${1} == "-n" ]] || unset DRYRUN
-    [[ ${1} == "-n" ]] || unset DRYRUN
+    #[[ -z $DRYRUN ]] && printf "${VERBOSE:+\n}[WARN] Performing UPDATE of remote config!\n${VERBOSE:+\n}"   >&2
+    [[ -n $DRYRUN ]] && printf "${VERBOSE:+\n}[DRYRUN] Performing UPDATE of remote config!\n${VERBOSE:+\n}" >&2
+    if [[ -n $VERBOSE ]] ; then
+        printf "\n*************************************\n"
+        printf "\n[INFO] Updating Remote Config Files:\n\n"
+        git status --no-renames --short | awk '/^ M/'
+        printf "\n*************************************\n\n"
+    fi >&2
     rsync ${DRYRUN:+-n} ${VERBOSE:+-v} -R -acb --copy-links \
         --exclude="*.swp" \
         --exclude="*.bkp" \
         --exclude="*.BAK" \
+        --exclude="*~" \
         --exclude="printer-$(date +%Y)*.cfg" \
-        ${LOCAL_BASEDIR%/}/./printer_data/ \
-        ${REMOTE_HOST}:printer_data/./config
+        ${LOCAL_BASEDIR%/}/./printer_data/$(git status --no-renames --short | awk '/^ M/ {print $NF}') \
+        ${KLIPPER_HOST}:printer_data/./config
 }
 
 
-
 #
-# notify of DRYRUN condition
+# Run Sync, backup / update
 #
-DRYRUN=${DRYRUN:=1}
-if [[ $DRYRUN == 0 ]] ; then
-    unset DRYRUN
-    printf "[WARN] This is NOT a dryrun! will overwrite files!\n" >&2
-else
-    printf "[INFO] This IS a dryrun! will NOT overwrite files!\n" >&2
-fi ; sleep 1
-
-
-case ${1%--} in
+case ${_opt} in
 
     "backup"|"receive"|"pull" )
         _backup_remote_config "${DRYRUN:+-n}" ;;
